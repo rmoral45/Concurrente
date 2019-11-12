04dd85e646f5cce41e070bee5d317ad3aa613c0f
rmoral45 <rmoral45@gmail.com>
2019-11-12 15:12:30 -0300

    Creamos directorio para rdp, json correctamente leido

--- src/main.java
+++ src/main.java
@@ -1,13 +1,14 @@
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import petriNet.*;
 
 public class main {
 
     public static void main(String[] args){
 
 
-        petriNet rdp = new petriNet("/home/ramiro/repos/Concurrente/src/test.json");
+        petriNet rdp = new petriNet("/home/ramiro/repos/Concurrente/src/petriNet/petriNet_parameters.json");
 
 
         return;
