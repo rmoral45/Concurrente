e5a483e4cbee9e97b0e40d93cecf81ad641826c1
rmoral45 <rmoral45@gmail.com>
2019-11-12 19:40:34 -0300

    agregamos buscadores de max y min value e index

--- src/petriNet/MathOperator.java
+++ src/petriNet/MathOperator.java
@@ -5,7 +5,7 @@ public class MathOperator {
         super();
     }
 
-    public int[] addVector(int size, int[] vector1, int[] vector2){
+    public static int[] addVector(int size, int[] vector1, int[] vector2){
 
         int[] result = new int[size];
 
@@ -15,7 +15,7 @@ public class MathOperator {
         return result;
     }
 
-    public int[] innerProdVector(int size, int[] vector1, int[] vector2){
+    public static int[] innerProdVector(int size, int[] vector1, int[] vector2){
 
         int[] result = new int[size];
 
@@ -25,7 +25,7 @@ public class MathOperator {
         return result;
     }
 
-    public int[] andVector(int size, int[] vector1, int[] vector2){
+    public static int[] andVector(int size, int[] vector1, int[] vector2){
 
         int[] result = new int[size];
 
@@ -35,7 +35,7 @@ public class MathOperator {
         return result;
     }
 
-    public int[][] matrixAdd(int size, int[][] vector1, int[][] vector2){
+    public static int[][] matrixAdd(int size, int[][] vector1, int[][] vector2){
 
         int[][] result = new int[size][size];
 
@@ -46,7 +46,7 @@ public class MathOperator {
         return result;
     }
 
-    public int[][] matrixProd(int size, int[][] vector1, int[][] vector2){
+    public static int[][] matrixProd(int size, int[][] vector1, int[][] vector2){
 
         int[][] result = new int[size][size];
 
@@ -57,14 +57,71 @@ public class MathOperator {
         return result;
     }
 
-    public int[] vectmatProd(int matrix_size, int vector_size, int [][] matrix, int[] vector){
+    public static int[] vectmatProd(int matrix_size, int vector_size, int [][] matrix, int[] vector){
 
-        int [] result = new int[vector_size];
+        int [] result = new int[matrix_size];
 
-        for(int i = 0; i < vector_size; i++)
-            for(int j = 0; j < matrix_size; j++)
+        for(int i = 0; i < matrix_size; i++)
+            for(int j = 0; j < vector_size; j++)
                 result[i] += matrix[i][j] * vector[j];
 
         return result;
     }
+
+    public static int getMaxIndex(int[] vector){
+
+        int size = vector.length;
+        int max_index = 0;
+        int max_value = 0;
+
+        for(int i = 0; i < size; i++){
+            if(vector[i] > max_value){
+                max_value = vector[i];
+                max_index = i;
+            }
+        }
+        return max_index;
+    }
+
+    public static int getMinIndex(int[] vector){
+
+        int size = vector.length;
+        int min_index = 0;
+        int min_value = 1000000;
+
+        for(int i = 0; i < size; i++){
+            if(vector[i] < min_value){
+                min_value = vector[i];
+                min_index = i;
+            }
+        }
+        return min_index;
+    }
+
+    public static int getMaxValue(int[] vector){
+
+        int size = vector.length;
+        int max_value = 0;
+
+        for(int i = 0; i < size; i++){
+            if(vector[i] > max_value){
+                max_value = vector[i];
+            }
+        }
+        return max_value;
+    }
+
+    public static int getMinValue(int[] vector){
+
+        int size = vector.length;
+        int min_value = 1000000;
+
+        for(int i = 0; i < size; i++){
+            if(vector[i] < min_value){
+                min_value = vector[i];
+            }
+        }
+        return min_value;
+    }
+
 }
