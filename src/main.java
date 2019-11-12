71ea08a4847ec81c6f62dc82979883109155aa0b
rmoral45 <rmoral45@gmail.com>
2019-11-11 21:38:09 -0300

    petriParser andando, falta parsear matriz

--- src/main.java
+++ src/main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class main {
+
+    public static void main(String[] args){
+
+
+        petriNet rdp = new petriNet("/home/ramiro/repos/Concurrente/src/test.json");
+
+
+        return;
+    }
 }
+
