ed8b6a5a9f70740ec2cf2bf654599d9af2b734b0
rmoral45 <rmoral45@gmail.com>
2019-11-15 17:36:53 -0300

    metodos documentados

--- src/petriNet/PetriNet.java
+++ src/petriNet/PetriNet.java
@@ -1,15 +1,17 @@
 package petriNet;
+import Parser.*;
 
 public class PetriNet {
 
     private int nplaces;
     private int ntransitions;
-    private int[][] incidenceMatrix;
+    //private int[][] incidenceMatrix;
+    public int [][] incidenceMatrix;
     private int[] mark_vector;
 
     public PetriNet(String fileName){
 
-        PetriParser parser = new PetriParser(fileName);
+        Parser parser = new Parser(fileName);
         this.nplaces = parser.getNplaces();
         this.ntransitions = parser.getNtransitions();
         this.incidenceMatrix = parser.getPetriNet_matrix();
@@ -17,41 +19,86 @@ public class PetriNet {
         //falta agregar arcos lectores y arcos inhibidores
     }
 
-    private int getNtransitions() {
+    int getNtransitions() {
         return ntransitions;
     }
 
-    private int getNplaces() {
+    int getNplaces() {
         return nplaces;
     }
 
-    private int[] getMark_vector() {
+    int[] getMark_vector() {
         return mark_vector;
     }
 
-    private int[][] getIncidenceMatrix() {
+    int[][] getIncidenceMatrix() {
         return incidenceMatrix;
     }
 
-    public boolean dispararTransicion(int transition){
+    /**
+     * habilitarTransicion - dispara una transicion SIN CAMBIAR EL ESTADO DE LA RED
+     * dispara la transicion pasada como argumento
+     *
+     * @param transition numero de transicion a disparar.
+     * @return int[} - retorna el vector de marcado obtenido al disparar la transicion.
+     */
+    public int[] probarDisparo(int transition){
 
         int [] vector_de_disparo = new int [ntransitions];
         vector_de_disparo[transition] = 1;
-        boolean flag = false;
 
-        int [] actual_marking = this.mark_vector;
-        int [] new_marking = new int[nplaces];
-
+        int [] new_marking;
         new_marking = MathOperator.vectmatProd(this.incidenceMatrix, vector_de_disparo);
-        for(int i = 0; i < nplaces; i++)
-            new_marking[i] = new_marking[i] + actual_marking[i];
 
+        return new_marking;
+    }
 
-        flag = (new_marking[transition] < 0) ? false : true;
+    /**
+     * dispararTransicion - verifica si la transicion es disparable, si lo es cambia el estado del sistema
+     * @param transition numero de transicion a disparar
+     * @return  true: si se pudo disparar la transicion
+     *          false: si no se pudo disparar la transicion
+     */
+    public boolean dispararTransicion(int transition){
 
-        if(flag)
+        int[] new_marking  = this.probarDisparo(transition);
+
+        for(int i = 0; i < new_marking.length; i++)
+            System.out.printf("NUEVO MARCADO - PROBAR DISPARO: Fila %d: %d\n", i, new_marking[i]);
+
+        new_marking = MathOperator.addVector(new_marking,this.mark_vector);
+
+        for(int i = 0; i < new_marking.length; i++)
+            System.out.printf("NUEVO MARCADO - ADD VECTOR: Fila %d: %d\n", i, new_marking[i]);
+
+        System.out.printf("El Resultado del hasnegative() es: %b", MathOperator.HasNegative(new_marking));
+
+        if(MathOperator.HasNegative(new_marking))
+            return false;
+        else {
             this.mark_vector = new_marking;
+            return true;
+        }
+    }
 
-        return flag;
+    /**
+     * obtenerSensibilidazas - verifica cuales de todas las transiciones de la red estan sensibilizadas
+     * @return int[]: vector con 1 en cuyas posiciones la transicion se encuentra sensibilizada, por ejemplo
+     *                si se tienen 6 transiciones y solo la 2 y la 5 estan sensibilizadas retorna [0,0,1,0,0,1]
+     */
+    public int[] obtenerSensibilizadas(){
+
+        int [] sensibilizadas = new int[this.ntransitions];
+        int [] tmp_vector;
+
+        for(int i = 0; i < this.ntransitions; i++) {
+
+            tmp_vector = probarDisparo(i);
+            if (MathOperator.HasNegative(tmp_vector))
+                sensibilizadas[i] = 0;
+            else
+                sensibilizadas[i] = 1;
+        }
+         return  sensibilizadas;
     }
 }
