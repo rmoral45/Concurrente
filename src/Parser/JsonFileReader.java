6860e8929c9c0fcbc2209b9134189671016f482d
rmoral45 <rmoral45@gmail.com>
2019-11-20 23:29:05 -0300

    renombramos al JsonParser por JsonFileReader

---
+++ src/Parser/JsonFileReader.java
@@ -0,0 +1,60 @@
+package Parser;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class JsonFileReader {
+
+    private PetriNetConfigurator pnConfigurator;
+    private ThreadTriggerConfigurator triggerConfigurator;
+
+    /* --------------- FOR TESTING ---------------*/
+    //DESCOMENTAR CUANDO SE CORRA EL TEST DE PETRINET CONFIGURATOR
+    public JsonObject petriNet_test;
+    //DESCOMENTAR CUANDO SE CORRA EL TEST DE THREADTRIGGER CONFIGURATOR
+    public JsonObject Triggers_test;
+
+    public JsonFileReader(String fileName) {
+
+        try (FileReader reader = new FileReader(fileName))
+        {
+            //--------------------------------------------------(petriNet)----------------------------------------------
+            //leemos json
+            JsonObject jsonObject = new JsonParser().parse(reader).getAsJsonObject();
+            //leemos la petriNet y creamos el configurator de la misma
+            JsonObject petriNet = jsonObject.getAsJsonObject("petriNet");
+            PetriNetConfigurator pnConfigurator = new PetriNetConfigurator(petriNet);
+
+            //--------------------------------------------------(Thread Triggers)---------------------------------------
+            //leemos json
+            JsonObject triggers = jsonObject.getAsJsonObject("Triggers");
+            //leemos los triggers y creamos el configurator de los mismos
+            ThreadTriggerConfigurator triggerConfigurator = new ThreadTriggerConfigurator(triggers);
+
+            /* --------------- FOR TESTING ---------------*/
+            //DESCOMENTAR CUANDO SE CORRA EL TEST DE PETRINET CONFIGURATOR
+            this.petriNet_test = jsonObject.getAsJsonObject("petriNet");
+            this.Triggers_test = jsonObject.getAsJsonObject("Triggers");
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public PetriNetConfigurator getPnConfigurator() {
+        return pnConfigurator;
+    }
+
+    public ThreadTriggerConfigurator getTriggerConfigurator() {
+        return triggerConfigurator;
+    }
+}
+
+
+
