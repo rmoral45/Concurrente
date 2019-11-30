a71b2e82583e4d83129f0a5b0b34a0dea69275c5
dabratte <abrattediego@gmail.com>
2019-11-30 11:01:29 -0300

    test T-invariantes done

--- log_files/parser/parse_log.py
+++ log_files/parser/parse_log.py
@@ -2,7 +2,9 @@
 from pdb import set_trace as bp
 import re
 import json
-LOG_FILE = '../finalv4.log'
+import numpy
+LOG_FILE = '../final.log'
+PETRI_FILE = '/home/dabratte/repos/Concurrente/src/petriNet/red_final.json'
 N_TRANSICIONES = 16
 def main():
 
@@ -27,6 +29,41 @@ def main():
 
                 for i in range(N_TRANSICIONES):
                         print('La transicion : ', i, ' se disparo ', transiciones.count(i))
+
+                remover_invariantes(transiciones,[7,0,2,3,4,15,5,6])
+                remover_invariantes(transiciones,[8,10,11,9])
+
+                probar_Tinv(transiciones, N_TRANSICIONES, marcados[-1])
+
+def remover_invariantes(ts, invariantes):
+        min_rep = len(ts)
+
+        #obtengo cantidad de invariantes completos
+        for inv in invariantes:
+                if ts.count(inv) < min_rep:
+                        min_rep = ts.count(inv)
+
+        #remuevo las secuencias invariantes
+        for inv in invariantes :
+                for k in range(min_rep):
+                        ts.remove(inv)
+
+def probar_Tinv(ts, ntrns,marca_log):
+        sigma = []
+        for i in range(ntrns):
+                sigma.append([ts.count(i)])
+
+        sigma = numpy.matrix(sigma)
+
+        with open(PETRI_FILE) as fd :
+                jobs = json.loads(fd.read())
+                matI = numpy.matrix(jobs['petriNet']['incidence_matrix'])
+                Mi   = numpy.matrix(jobs['petriNet']['init_marking'])
+                Mf   = numpy.transpose((matI * sigma)) + Mi
+                Mlog = numpy.matrix(marca_log)
+                print('Marcado final atravez de invariantes : \n',Mf,'\n')
+                print('Marcado final del log :\n', Mlog, '\n')
+                print('Comparacion : ', Mlog == Mf)
 
 
 
