daddf9f32e9e56beb1520109d542f5c15ee89ba7
rmoral45 <rmoral45@gmail.com>
2019-12-03 11:10:25 -0300

    subiendo diagramas

--- src/Monitor/MonitorV2Test.java
+++ src/Monitor/MonitorV2Test.java
@@ -16,7 +16,7 @@ public class MonitorV2Test {
         JsonFileReader fr = new JsonFileReader("/home/ramiro/repos/Concurrente/src/petriNet/red_final.json");
 
         //PetriNetConfigurator pnConf = new PetriNetConfigurator(fr.petriNet_test);
-        PetriNet rdp = new PetriNet(fr.getPnConfigurator(), true,"/home/ramiro/repos/Concurrente/log_files/finalv9.log");
+        PetriNet rdp = new PetriNet(fr.getPnConfigurator(), true,"/home/ramiro/repos/Concurrente/log_files/finalv10.log");
         Politica policy = new Politica(fr.getPolicyConfigurator());
         MonitorV2 monitor = new MonitorV2(rdp.getNtransitions(), policy,rdp);
         ThreadTriggerConfigurator ttConf = fr.getTriggerConfigurator();
