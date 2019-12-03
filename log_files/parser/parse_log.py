e3181d8b4040e935c13894e78fc33d272d9cae12
dabratte <abrattediego@gmail.com>
2019-12-03 15:20:30 -0300

    fix inavariantes

--- log_files/parser/parse_log.py
+++ log_files/parser/parse_log.py
@@ -3,9 +3,10 @@ from pdb import set_trace as bp
 import re
 import json
 import numpy
+import copy
 LOG_FILE = '../finalPresentacion.log'
 PETRI_FILE = '/home/dabratte/repos/Concurrente/src/petriNet/red_final.json'
-N_TRANSICIONES = 16
+N_TRANSICIONES = ['T0','T1','T2','T3','T4','T5','T6','T7','T8','T9','T10','T11','T12','T13','T14','T15']
 def main():
 
         with open(LOG_FILE) as fd:
@@ -17,29 +18,40 @@ def main():
                         jslist.append(json.loads(item))
 
                 #genero lista de disparos y marcados
-
                 transiciones = []
                 marcados = []
                 for js in jslist :
                         transiciones.append(js['disparo'])
                         marcados.append(js['marcado'])
 
-                for i in range(N_TRANSICIONES):
+                for i in N_TRANSICIONES :
                         if transiciones.count(i) < 1:
                                 print('La transicion : ', i, ' no se disparo nunca\n')
 
-                for i in range(N_TRANSICIONES):
+                for i in N_TRANSICIONES :
                         print('La transicion : ', i, ' se disparo ', transiciones.count(i))
+
                 for i in range(len(transiciones)):
-                        if transiciones[i] == 1 :
-                                transiciones[i] = 0
+                        if transiciones[i] == 'T1' :
+                                transiciones[i] = 'T0'
 
-                remover_invariantes(transiciones,[7,0,2,3,4,15,5,6])
-                remover_invariantes(transiciones,[8,10,11,9])
+                
+                t_inv_mat = []
+                t_inv_mat.append(remover_invariantes(transiciones,['T7','T0','T2','T3','T4','T15','T5','T6']))
+                t_inv_mat.append(remover_invariantes(transiciones,['T8','T10','T11','T9']))
+                clean = []
+                for i in t_inv_mat:
+                        clean += i.split('T')
+                for i in range(len(transiciones)) :
+                        if transiciones[i] in ['T7','T0','T2','T3','T4','T15','T5','T6','T8','T10','T11','T9'] :
+                                transiciones[i] = ''
 
-                probar_Tinv(transiciones, N_TRANSICIONES, marcados[-1])
+                clean += ''.join(transiciones).split('T')
+                bp()
+                probar_Tinv(clean, marcados[-1])
 
 def remover_invariantes(ts, invariantes):
+        '''
         min_rep = len(ts)
 
         #obtengo cantidad de invariantes completos
@@ -51,11 +63,21 @@ def remover_invariantes(ts, invariantes):
         for inv in invariantes :
                 for k in range(min_rep):
                         ts.remove(inv)
-
-def probar_Tinv(ts, ntrns,marca_log):
+        '''
+        nv = copy.deepcopy(ts)
+        for i in range(len(nv)):
+                if nv[i] not in invariantes:
+                        nv[i] = ''
+        union = ''.join(nv)
+        inv = ''.join(invariantes)
+        union = union.replace(inv, '')
+        return union
+        
+def probar_Tinv(ts,marca_log):
         sigma = []
-        for i in range(ntrns):
-                sigma.append([ts.count(i)])
+        for i in N_TRANSICIONES:
+                sigma.append([ts.count(i[1:])])
+        bp()
 
         sigma = numpy.matrix(sigma)
 
